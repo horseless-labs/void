c93b17d4840abb6d08e8bab2d0b80b0cfc980959
mireles <mireles@horseless.io>
2024-08-02 13:21:06 -0400

    Add navbar.html, start login/registration

--- base/views.py
+++ base/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-# Create your views here.
 def home(request):
     return render(request, "base/home.html")
+
+def loginPage(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or password does not exist.")
+
+    context = {"page": page}
+    return render(request, "base/login_register.html", context)
+
+
+# TODO: come back to this after handling login/registration
+def chat(request):
+    pass
